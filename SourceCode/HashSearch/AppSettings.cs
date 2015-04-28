c63ab88b401b0aaa693f2794c6456bdb32888d8a
Ehryk <rhaistlin@gmail.com>
2015-04-28 03:02:38 -0500

    HashSearch: Added UpdateInterval Setting

--- SourceCode/HashSearch/AppSettings.cs
+++ SourceCode/HashSearch/AppSettings.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 
 namespace HashSearch
 {
@@ -9,6 +10,7 @@ namespace HashSearch
         private static readonly string connectionString = ConfigurationManager.ConnectionStrings["Main"] != null ? ConfigurationManager.ConnectionStrings["Main"].ConnectionString : null;
 
         private static readonly string defaultHashAlgorithm = ConfigurationManager.AppSettings["DefaultHashAlgorithm"];
+        private static readonly string updateInterval = ConfigurationManager.AppSettings["UpdateInterval"];
 
         private static readonly string sp_Similarity_Insert = ConfigurationManager.AppSettings["SP_Similarity_Insert"];
         private static readonly string sp_Search_Start = ConfigurationManager.AppSettings["SP_Search_Start"];
@@ -21,6 +23,7 @@ namespace HashSearch
         public static string ConnectionString { get { return connectionString ?? "Data Source=localhost;Initial Catalog=HashSearch;User ID=HashSearch;Password=password;"; } }
 
         public static string DefaultHashAlgorithm { get { return defaultHashAlgorithm ?? "MD5"; } }
+        public static TimeSpan UpdateInterval { get { return TimeSpan.Parse(updateInterval ?? "00:01:00"); } }
 
         public static string SP_Similarity_Insert { get { return sp_Similarity_Insert ?? "HashSimilarity_Insert"; } }
         public static string SP_Search_Start { get { return sp_Search_Start ?? "HashSearch_Start"; } }
