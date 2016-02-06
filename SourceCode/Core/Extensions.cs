5bbbe8de4c78b99f83248386aaadcae038764bc1
Ehryk <rhaistlin@gmail.com>
2016-02-05 23:58:22 -0600

    Extensions: Adding GetDecimalBytes from SO question, implementing GetOctalBytes

--- SourceCode/Core/Extensions.cs
+++ SourceCode/Core/Extensions.cs
@@ -133,30 +133,22 @@ namespace Core
 
             octal = octal.TrimStart('0');
 
-            byte[] octalDigits = Enumerable.Range(0, octal.Length)
-                 .Select(x => Convert.ToByte(octal.Substring(x, 1), 8))
-                 .ToArray();
+            BitArray bits = new BitArray(octal
+                .Reverse()
+                .SelectMany(x =>
+                    {
+                        byte value = (byte)(x - '0');
+                        return new bool[] { (value & 0x01) == 1, (value & 0x02) == 2, (value & 0x04) == 4 };
+                    })
+                .ToArray());
 
-            int length = octalDigits.Length % 8 == 0 ? octalDigits.Length * 3 / 8 : octalDigits.Length * 3 / 8 + 1;
-            BitArray ba = new BitArray(length * 8);
-            
-            for (int i = octalDigits.Length - 1; i >= 0; i--)
-            {
-                ba[(octalDigits.Length - i) * 3 - 3] = (octalDigits[i] & 0x01) == 1;
-                ba[(octalDigits.Length - i) * 3 - 2] = (octalDigits[i] & 0x02) == 2;
-                ba[(octalDigits.Length - i) * 3 - 1] = (octalDigits[i] & 0x04) == 4;
-            }
+            byte[] bytes = new byte[bits.Length / 8 + 1];
+            bits.CopyTo(bytes, 0);
 
-            byte[] result = new byte[length];
-
-            ba.CopyTo(result, 0);
-
-            if (result[length - 1] == 0x00)
-            {
-                result = result.Take(result.Length - 1).ToArray();
-            }
-
-            return result.Reverse().ToArray();
+            if (bytes[bytes.Length - 1] == 0x00)
+                bytes = bytes.Take(bytes.Length - 1).ToArray();
+                    
+            return bytes;
         }
 
         public static byte[] GetBinaryBytes(this string binary, bool preTrimmed = false)
@@ -185,10 +177,64 @@ namespace Core
                     dec = dec.Substring(3);
             }
 
-            return Enumerable.Range(0, dec.Length)
-                 .Select(x => Convert.ToByte(dec.Substring(x, 1), 10))
-                 .Reverse()
-                 .ToArray();
+            return dec
+                // interpret the string as a list of digits with position
+                .Reverse()
+                // transfer from list of positioned digits to list of actual bit positions,
+                // by repeatedly multiplying with 10
+                // the resulting bits need to be added for the final result
+                .SelectMany((x, i) =>
+                {
+                    // digit value
+                    var val1 = x - '0';
+                    var res1 = new List<int>();
+                    // to bit positions, as if it was the first digit
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if ((val1 & (1 << j)) != 0) res1.Add(j);
+                    }
+                    // to absolute bit positions, taking the digit position into account
+                    for (int j = 1; j <= i; j++)
+                    {
+                        var res = new List<int>();
+                        // multiply by 10, until actual position is reached
+                        foreach (var item in res1)
+                        {
+                            res.Add(item + 1);
+                            res.Add(item + 3);
+                        }
+                        // compress bits
+                        res1 = res.Aggregate(new HashSet<int>(), (set, i1) =>
+                        {
+                            // two bits in the same position add up to one bit in a higher position
+                            while (set.Contains(i1))
+                            {
+                                set.Remove(i1);
+                                i1++;
+                            }
+                            set.Add(i1);
+                            return set;
+                        }).ToList();
+                    }
+                    return res1;
+                }).
+                // final elimination of duplicate bit indices
+                Aggregate(new HashSet<int>(), (set, i) =>
+                {
+                    while (set.Contains(i))
+                    {
+                        set.Remove(i);
+                        i++;
+                    }
+                    set.Add(i);
+                    return set;
+                })
+                // transfer bit positions into a byte array - lowest bit is the last bit of the first byte
+                .Aggregate(new byte[dec.Length / 2], (res, bitpos) =>
+                {
+                    res[bitpos / 8] |= (byte)(1 << (bitpos % 8));
+                    return res;
+                });
         }
 
         public static byte[] AddOne(this byte[] input)
